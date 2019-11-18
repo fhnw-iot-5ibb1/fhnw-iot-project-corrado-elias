7b5457742731b2498eb22d711fe67bb9514e75c6
Elias Schorr <elias.schorr@fhnw.ch>
2019-11-18 13:35:22 +0100

    add some more logic

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
+import datetime
 import time
 
 import grovepi
 
 # Connect the Grove Ultrasonic Ranger to digital port D4 SIG,NC,VCC,GND
-ultrasonic_ranger = 4
+ultrasonic_ranger_1 = 4
+ultrasonic_ranger_2 = 4
 # Connect the Grove Buzzer to digital port D8 SIG,NC,VCC,GND
 buzzer = 8
 # Connect first LED in Chainable RGB LED chain to digital port D7; In: CI,DI,VCC,GND ; Out: CO,DO,VCC,GND
@@ -20,6 +22,22 @@ grovepi.chainableRgbLed_init(ledPin, numleds)
 alarmActive = False
 buzzerActive = 0
 testColor = 0
+last_ultrasonic_ranger_1 = False
+last_ultrasonic_ranger_2 = False
+# TODO test trigger distance
+trigger_distance = 120
+
+
+def isNowInTimePeriod(startTime, endTime, nowTime):
+    if startTime < endTime:
+        return nowTime >= startTime and nowTime <= endTime
+    else:  # Over midnight
+        return nowTime >= startTime or nowTime <= endTime
+
+
+def isBadTime():
+    # Test case when range crosses midnight
+    return isNowInTimePeriod(time(23, 0), time(5, 00), datetime.utcnow().time())
 
 
 def triggerAlarm():
@@ -32,28 +50,54 @@ def triggerAlarm():
     grovepi.chainableRgbLed_test(ledPin, numleds, testColor)
 
 
+def entry():
+    global alarmActive
+    # TODO send to firebase or whatever
+    print("entry")
+    # TODO only trigger based on time
+    if (isBadTime()):
+        alarmActive = True
+
+
+def exit():
+    global alarmActive
+    # TODO send to firebase or whatever
+    print("exit")
+    alarmActive = False
+
+
 def loop():
+    global last_ultrasonic_ranger_1, last_ultrasonic_ranger_2
     while True:
         try:
             # Read distance value from Ultrasonic
-            print(grovepi.ultrasonicRead(ultrasonic_ranger))
-            # TODO add if condition if range is less than 1.20m or something like that
-            # TODO save the condition -> if first ranger 1 or ranger 2
-            # TODO we could cange the logic based on the time e.g. trigger alarm anyway if time is 23:00 - 5:00
+            print(grovepi.ultrasonicRead(ultrasonic_ranger_1))
+            print(grovepi.ultrasonicRead(ultrasonic_ranger_2))
+
+            new_ultrasonic_ranger_1 = grovepi.ultrasonicRead(ultrasonic_ranger_1) < 120
+            new_ultrasonic_ranger_2 = grovepi.ultrasonicRead(ultrasonic_ranger_2) < 120
+            if last_ultrasonic_ranger_1 and new_ultrasonic_ranger_2 and not last_ultrasonic_ranger_2:
+                entry()
+            elif last_ultrasonic_ranger_2 and new_ultrasonic_ranger_1 and not last_ultrasonic_ranger_1:
+                exit()
+            last_ultrasonic_ranger_1 = new_ultrasonic_ranger_1
+            last_ultrasonic_ranger_2 = new_ultrasonic_ranger_2
+
             if alarmActive:
                 triggerAlarm()
-            # and send the data in,out detection anyway
+            # TODO test time to sleep
             time.sleep(.5)
 
         except KeyboardInterrupt:
+            print("KeyboardInterrupt")
             grovepi.digitalWrite(buzzer, 0)
             grovepi.chainableRgbLed_test(ledPin, numleds, 0)
             break
         except TypeError:
-            print("Error")
+            print("TypeError")
             break
         except IOError:
-            print("Error")
+            print("IOError")
             break
 
 
