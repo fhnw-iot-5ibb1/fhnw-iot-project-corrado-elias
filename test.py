2019cce53cafe10ff2be34d6897701d2f44a2e2e
Elias Schorr <elias.schorr@fhnw.ch>
2019-11-25 14:08:59 +0100

    update test.py

--- test.py
+++ test.py
@@ -2,18 +2,14 @@ import time
 
 import grovepi
 
-# Connect the Grove Buzzer to digital port D8
-# SIG,NC,VCC,GND
-buzzer = 5
-
 if __name__ == '__main__':
     for i in range(0, 20):
-        print(i, i ** 2)
+        print(i)
         grovepi.pinMode(i, "OUTPUT")
-        grovepi.digitalWrite(buzzer, 1)
+        grovepi.digitalWrite(i, 1)
         print('start')
         time.sleep(1)
         # Stop buzzing for 1 second and repeat
-        grovepi.digitalWrite(buzzer, 0)
+        grovepi.digitalWrite(i, 0)
         print('stop')
         time.sleep(1)
