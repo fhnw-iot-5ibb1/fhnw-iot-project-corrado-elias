02b539771095168025befff32876537312574bec
Elias Schorr <elias.schorr@fhnw.ch>
2019-11-25 15:07:36 +0100

    next try

--- test2.py
+++ test2.py
@@ -5,14 +5,22 @@ from grove.gpio import GPIO
 led = GPIO(12, GPIO.OUT)
 button = GPIO(22, GPIO.IN)
 
+
+def doIt(i: int):
+    print(i)
+    grovepi = led = GPIO(i, GPIO.OUT)
+    grovepi.write(1)
+    print('start')
+    time.sleep(1)
+    # Stop buzzing for 1 second and repeat
+    grovepi.write(0)
+    print('stop')
+    time.sleep(1)
+
+
 if __name__ == '__main__':
-    for i in range(0, 30):
-        print(i)
-        grovepi = led = GPIO(i, GPIO.OUT)
-        grovepi.write(1)
-        print('start')
-        time.sleep(1)
-        # Stop buzzing for 1 second and repeat
-        grovepi.write(0)
-        print('stop')
-        time.sleep(1)
+    doIt(16)
+    doIt(5)
+    doIt(0)
+    doIt(2)
+    doIt(4)
